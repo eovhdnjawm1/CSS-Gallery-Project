e55c52689280d9fab79059e6d70af9434502b2c0
YoungJinGo <skyyj32@naver.com>
2022-06-06 02:43:52 +0900

    Feat 3D Rotate Setting

---
+++ src/components/3dCom/Rotate3D.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+let Rotate3D = styled.div`
+
+`
+
+function Rotate3DCom() {
+	return(
+		<>
+		<Rotate3D>
+
+		</Rotate3D>
+		</>
+	)
+}
+
+export default Rotate3D
