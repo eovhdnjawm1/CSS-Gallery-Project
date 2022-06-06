56c5c8dd89e76996ef94cc527edb206c02933d96
YoungJinGo <skyyj32@naver.com>
2022-06-06 18:38:36 +0900

    Feat EffectRollup test, 코드 업데이트, 이미지 업로드

--- src/components/Route3D.js
+++ src/components/Route3D.js
@@ -3,6 +3,7 @@ import Card3D from './3dCom/Card3D.js'
 import FlipCard from './3dCom/FlipCard';
 import NeonMovingButton from './3dCom/NeonMovingButton';
 import Rotate3D from './3dCom/Rotate3D.js';
+import EffectRollupContent from './EffectsCom/EffectsRollup.js';
 
 function Route3D() {
 
@@ -10,10 +11,11 @@ function Route3D() {
 	return (
 		<>
 			<Routes>
-				<Route path="/detail/0" element={<Rotate3D />} />
+				<Route path="/detail/0" element={<EffectRollupContent />} />
 				<Route path="/detail/1" element={<Card3D />} />
 				<Route path="/detail/2" element={<FlipCard />} />
 				<Route path="/detail/3" element={<NeonMovingButton />} />
+				<Route path="/detail/4" element={<Rotate3D />} />
 			</Routes>
 		</>
 	)
